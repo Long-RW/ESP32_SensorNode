5b303d12dd67f92ab27ef2a18ab27bcd4eb33a6f
Long-RW <longx8qd@gmail.com>
2022-07-26 01:10:25 +0700

    Fix Delay Bug

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,78 @@
 #include <string.h>
 #define PIR 16
 
-// Replace the next variables with your SSID/Password combination
+/*****************************************Private Variable***************************************************************/
+//Wifi Config Variables
 const char* ssid = "Song Quynh";
 const char* password = "songquynh25042112";
+
+//Interrupt Variables
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
+//Sensor Read Variables
 char temp[5];
-// Add your MQTT Broker IP address, example:
+int counter = 1;
+bool sensorState = 0;
+// MQTT Conection Variables
 const char* mqtt_server = "192.168.1.41";
 unsigned long timeNow = 0;
-int buff = 0;
-int counter = 0;
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
+/***********************************************Private Macro*******************************************************************/
+
+void IRAM_ATTR SensoHIGH();
+void reconnect(); 
+void setup_wifi();
+void callback(char* topic, byte* message, unsigned int length);
+/********************************************************************************************************************************/
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIR, INPUT);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  attachInterrupt(digitalPinToInterrupt(PIR), SensoHIGH, RISING);
+}
+
+void loop() 
+{
+  if(sensorState)
+  {
+    if(counter == 1)
+    {
+      counter = 0;
+    }
+    else if (counter == 0)
+    {
+      counter = 1;
+    }
+    sprintf(temp,"%d",counter);
+    if (!client.connected()) 
+    {
+      reconnect();
+    }
+    client.loop();
+    long now = millis();
+    if (now - lastMsg > 100) 
+    {
+      lastMsg = now;
+      client.publish("Sensor/PIR1", temp);
+    }
+    sensorState = 0;
+  }
+  
+}
+
+/***********************************************Private Function*******************************************************************/
+void IRAM_ATTR SensoHIGH()
+{
+  portENTER_CRITICAL_ISR(&mux);     //Phần quan trọng cần được bảo vệ khỏi mọi truy cập đồng thời để thay đổi nó
+  sensorState = 1;
+  portEXIT_CRITICAL_ISR(&mux);    //Cho phép tiếp tục chạy các Task khác
+}
 
 void setup_wifi() {
   Serial.print("Connecting to ");
@@ -53,7 +111,7 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP32Client")) {
+    if (client.connect("Sensor1")) {
       Serial.println("connected");
       // Subscribe
       client.subscribe("test");
@@ -64,48 +122,5 @@ void reconnect() {
       // Wait 5 seconds before retrying
       delay(1000);
     }
-  }
-}
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(PIR, INPUT);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-
-}
-
-void loop() 
-{
-  buff = digitalRead(PIR);
-  
-  if(buff)
-  {
-    if(counter == 0)
-    {
-      counter = 1;
-    }
-    else if (counter == 1)
-    {
-      counter = 0;
-    }
-    sprintf(temp,"%d",counter);
-    if (!client.connected()) 
-    {
-      reconnect();
-    }
-    client.loop();
-    long now = millis();
-    if (now - lastMsg > 100) 
-    {
-      lastMsg = now;
-      client.publish("Sensor/PIR2", temp);
-    }
-  }
-
-  while(buff != 0)
-  {
-    buff = digitalRead(PIR);
   }
 }
